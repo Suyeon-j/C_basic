994c1db1a38f1dee7a1a26fa79adb2719f5b7bae
Suyeon-j <66247203+Suyeon-j@users.noreply.github.com>
2024-06-18 03:33:58 +0900

    Create printf.py

---
+++ printf.py
@@ -0,0 +1,14 @@
+#include <stdio.h> //자기소개 하기
+int main()
+{
+	printf("안녕하세요!\n");
+	printf("저는 000입니다.\n");
+
+  int num1 = 1, num2 = 2, num3 = -8;
+
+	printf("%d\n", sizeof(12.5));
+	printf("%f\n", (float)num1);
+
+	return 0;
+
+}
